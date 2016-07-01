abe4a69633fffda7a99e051b00aad00925c2f734
Jackson Argo <ignition.argo@gmail.com>
2016-07-01 21:49:23 +0000

    Created login page

--- app/controllers/feats_controller.rb
+++ app/controllers/feats_controller.rb
@@ -1,5 +1,8 @@
 class FeatsController < ApplicationController
+
+
   def index
+  redirect_to "/login" unless session[:user_id]
     @feats = Feat.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-#  root 'welcome#index'
+  root 'welcome#index'
 
 #  resources :articles do
 #    resources :comments
@@ -24,12 +24,11 @@ Rails.application.routes.draw do
   resources :players
 
 
-get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-root :to => "users#new"
-resources :users
-resources :sessions
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  resources :users
+  resources :sessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
